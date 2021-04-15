00c7db5e09334dbd22b6446d66c02a40745b292a
Kalstein <67362504+Kalstein@users.noreply.github.com>
2021-04-15 14:45:30 +0900

    Updata Hello.java : Hi~

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello");
-		System.out.println("world");
-		System.out.println("!!");
+		System.out.println("World");
+		System.out.println("Hi~");
 	}
 
 }
